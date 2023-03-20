d25739dd17a0d772552264fee348bb66b5c56130
kor-ash <109964592+kor-ash@users.noreply.github.com>
2023-03-20 21:14:55 +0900

    COSE457 SWProject Vector Complete

--- src/Container.js
+++ src/Container.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { update } from 'react-spring';
 
 const Vector = ({ x, y, dx, dy, ctx }) => {
 
@@ -7,13 +8,18 @@ const Vector = ({ x, y, dx, dy, ctx }) => {
     ctx.moveTo(x, y);
     ctx.lineTo(x + dx, y + dy);
     ctx.stroke();
+    ctx.closePath()
 }
+
 const Container = ({ curTool, handleUtensil, utensil }) => {
-    console.log(curTool, utensil['tool'])
     const canvasRef = useRef(null);
+    const dupRef = useRef(null);
+    const dupContextRef = useRef(null);
     const contextRef = useRef(null);
     const [isDrawing, setIsDrawing] = useState(false);
     const [ctx, setCtx] = useState();
+    const [dupCtx, setDupCtx] = useState();
+    const [startPoint, setStartPoint] = useState({ x: 0, y: 0 })
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -24,6 +30,17 @@ const Container = ({ curTool, handleUtensil, utensil }) => {
         context.lineWidth = 15;
         contextRef.current = context;
         setCtx(contextRef.current);
+
+        //to duplicate code
+
+        const dupCanvas = dupRef.current;
+
+        dupCanvas.width = window.innerHeight * 0.65;
+        dupCanvas.height = window.innerHeight * 0.4;
+        const dupContext = dupCanvas.getContext("2d");
+        dupContextRef.current = dupContext;
+        setDupCtx(dupContextRef.current);
+
     }, []);
 
     useEffect(() => {
@@ -36,6 +53,7 @@ const Container = ({ curTool, handleUtensil, utensil }) => {
     }, [utensil]);
 
     const startDrawing = ({ nativeEvent }) => {
+
         const { offsetX, offsetY } = nativeEvent;
         if (ctx) {
             setIsDrawing(true);
@@ -43,13 +61,35 @@ const Container = ({ curTool, handleUtensil, utensil }) => {
                 ctx.beginPath();
                 ctx.moveTo(offsetX, offsetY);
             }
+            else {
+                setStartPoint({ x: offsetX, y: offsetY })
+            }
         }
     };
-
-    const endDrawing = () => {
-        setIsDrawing(false);
+    const updateVector = ({ nativeEvent }) => {
+        const { offsetX, offsetY } = nativeEvent;
+        const dx = offsetX - startPoint.x;
+        const dy = offsetY - startPoint.y;
+        Vector({ x: startPoint.x, y: startPoint.y, dx, dy, ctx });
     };
+    const endDrawing = ({ nativeEvent }) => {
+        //마우스에서 손 뗌, 이때 최종적으로 남아야함
 
+        const dupCanvas = dupRef.current;
+        const dupContext = dupCanvas.getContext("2d");
+        const canvas = canvasRef.current;
+
+        const { offsetX, offsetY } = nativeEvent;
+        const dx = offsetX - startPoint.x;
+        const dy = offsetY - startPoint.y;
+        setIsDrawing(false);
+        if (curTool === "vector") {
+            updateVector({ nativeEvent })
+        }
+        dupContext.drawImage(canvas, 0, 0);
+        ctx.drawImage(dupCanvas, 0, 0);
+        ctx.closePath();
+    };
     const drawing = ({ nativeEvent }) => {
         const { offsetX, offsetY } = nativeEvent;
         if (ctx) {
@@ -60,16 +100,15 @@ const Container = ({ curTool, handleUtensil, utensil }) => {
                 }
             } else if (utensil["tool"] === "vector") {
                 if (isDrawing) {
-                    const { offsetX: startX, offsetY: startY } = nativeEvent;
-                    const dx = offsetX - startX;
-                    const dy = offsetY - startY;
-                    //ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-                    Vector({ x: startX, y: startY, dx, dy, ctx });
+                    const newDx = offsetX - startPoint.x;
+                    const newDy = offsetY - startPoint.y;
+                    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+                    ctx.drawImage(dupRef.current, 0, 0);
+                    Vector({ x: startPoint.x, y: startPoint.y, dx: newDx, dy: newDy, ctx });
                 }
             }
         }
     };
-
     return (
         <div>
             <canvas
@@ -77,9 +116,9 @@ const Container = ({ curTool, handleUtensil, utensil }) => {
                 onMouseDown={startDrawing}
                 onMouseUp={endDrawing}
                 onMouseMove={drawing}
-                onMouseLeave={endDrawing}
                 style={{ border: "solid" }}
             ></canvas>
+            <canvas id="c2" ref={dupRef} style={{ border: "solid", display: "none" }}></canvas>
         </div>
     );
 };
